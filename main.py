b6b86e65ffb0458334a5ab3f7a36bc339e78ae2c
chloekoh2021 <chloe.koh.2021@scis.smu.edu.sg>
2024-01-21 00:11:00 +0800

    window capture function working

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import numpy as np
 import cv2 as cv
-img = cv.imread("p.png")
+import pyautogui
+from time import time
+import win32gui, win32ui ,win32con
+from windowcapture import WindowCapture
 
-cv.imshow("Display window", img)
-k = cv.waitKey(0) # Wait for a keystroke in the window
+
+wincap = WindowCapture('Voice & Video | User Settings - Discord')
+
+loop_time = time()
+while True:
+    screenshot = wincap.get_screenshot()
+
+    cv.imshow('Computer Vision', screenshot)
+
+    # debug the loop rate
+    print('FPS {}'.format(1 / (time() - loop_time)))
+    loop_time = time()
+    
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+
+wincap.list_window_names()
+
+print('Done.')
